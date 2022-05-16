de3f40089a430b67ed697ad67b5cb1d9eef601be
Christian Cito <citochris@gmail.com>
2022-05-16 17:03:36 +0200

    Trim client id

--- app/lib/getStreamInfo.server.ts
+++ app/lib/getStreamInfo.server.ts
@@ -47,13 +47,13 @@ export const getStreamInfo = async () => {
     return await Promise.all([
       fetch(`https://api.twitch.tv/helix/streams?first=1&user_id=${207813352}`, {
         headers: {
-          "Client-Id": process.env.TWITCH_CLIENT_ID ?? "",
+          "Client-Id": process.env.TWITCH_CLIENT_ID?.trim() ?? "",
           "Authorization": `Bearer ${process.env.TWITCH_ACCESS_TOKEN ?? ""}`,
         },
       }).then((res) => res.json()) as unknown as StreamInfo,
       fetch(`https://api.twitch.tv/helix/schedule?broadcaster_id=${207813352}`, {
         headers: {
-          "Client-Id": process.env.TWITCH_CLIENT_ID ?? "",
+          "Client-Id": process.env.TWITCH_CLIENT_ID?.trim() ?? "",
           "Authorization": `Bearer ${process.env.TWITCH_ACCESS_TOKEN ?? ""}`,
         },
       }).then((res) => res.json())  as unknown as StreamSchedule,
