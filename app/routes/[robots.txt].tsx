a12ddb0344f148201c47163070fcbd97b5609139
Christian Cito <citochris@gmail.com>
2022-05-19 22:48:52 +0200

    stop google from indexing all variations of tags with robots.txt

--- app/routes/[robots.txt].tsx
+++ app/routes/[robots.txt].tsx
@@ -1,14 +1,22 @@
-export const loader = () => {
+import { prisma } from "~/utils/prisma.server";
+
+export const loader = async () => {
+  const tags = await prisma.tag.findMany();
   // handle "GET" request
   // set up our text content that will be returned in the response
   const robotText = `
-        User-agent: Googlebot
-        Disallow: /nogooglebot/
-    
-        User-agent: *
-        Allow: /
-    
-        Sitemap: http://hasanhub.com/sitemap.xml
+User-agent: Googlebot
+Disallow: /nogooglebot/
+Allow: /$    
+User-agent: *
+${tags
+  .map((tag) => {
+    return `Allow: /tags/${tag.slug}/`;
+  })
+  .join("\n")}
+Disallow: /
+
+Sitemap: http://hasanhub.com/sitemap.xml
         `;
   // return the text content, a status 200 success response, and set the content type to text/plain
   return new Response(robotText, {
