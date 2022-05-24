99935db73848ad49134431771f7db71645bf6bf7
Christian Cito <citochris@gmail.com>
2022-05-24 11:26:22 +0200

    Final update robots.txt

--- app/routes/[robots.txt].tsx
+++ app/routes/[robots.txt].tsx
@@ -4,7 +4,7 @@ export const loader = async () => {
   const tags = await prisma.tag.findMany();
   const slugCombinations = tags
     .map((tag) => {
-      return `Allow: /tags/${tag.slug}/`;
+      return `Allow: /tags/${tag.slug}`;
     })
     .join("\n");
 
@@ -17,28 +17,20 @@ Allow: /build/*.gif`;
 
   const allowPlausible = `Allow: /stats/js/script.js`;
 
-  const block = `${slugCombinations}
-${allowPlausible}
-${allowAssets}`;
+  const block = `Sitemap: https://hasanhub.com/sitemap.xml
+Allow: /$
+Allow: /tags/*/$
+Disallow: /tags/*/*`;
 
   const robotText = `
 User-agent: Googlebot
-Sitemap: https://hasanhub.com/sitemap.xml
-Allow: /$
 ${block}
-Disallow: /
 
 User-agent: Yandex
-Sitemap: https://hasanhub.com/sitemap.xml
-Allow: /$
 ${block}
-Disallow: /
 
 User-agent: *
-Sitemap: https://hasanhub.com/sitemap.xml
-Allow: /$
 ${block}
-Disallow: /
         `;
   // return the text content, a status 200 success response, and set the content type to text/plain
   return new Response(robotText, {
