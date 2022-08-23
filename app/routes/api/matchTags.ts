b3039ca17cde7d43f64e62e4e500d70d70f3c442
Christian Cito <citochris@gmail.com>
2022-08-23 21:24:26 +0200

    Batch update transactions

--- app/routes/api/matchTags.ts
+++ app/routes/api/matchTags.ts
@@ -2,7 +2,7 @@ import { prisma } from "~/utils/prisma.server";
 import { json } from "@remix-run/node";
 import { debug } from "~/utils/debug.server";
 import { matchTagWithVideos } from "~/sync/services/matching";
-import { Video } from "@prisma/client";
+import type { Tag } from "@prisma/client";
 
 export async function loader({ params }) {
   try {
@@ -12,7 +12,7 @@ export async function loader({ params }) {
     ]);
 
     let taggedVideos: { [key: string]: number } = {};
-    await prisma.$transaction(
+    await batchTransactions(
       tags.map((tag) => {
         const filteredVideos = videos.filter((video) => {
           if (tag.lastedMatchedAt === null || video.publishedAt === null) {
@@ -29,21 +29,31 @@ export async function loader({ params }) {
 
         debug(`${tag.name} matched ${matchedVideos.length} videos`);
 
-        return prisma.tag.update({
-          where: { id: tag.id },
-          data: {
-            lastedMatchedAt: new Date(),
-            videos: {
-              createMany: {
-                data: matchedVideos.map((matchedVideo) => ({
-                  videoId: matchedVideo.id,
-                })),
-                skipDuplicates: true,
+        if (matchedVideos.length > 0) {
+          return prisma.tag.update({
+            where: { id: tag.id },
+            data: {
+              lastedMatchedAt: new Date(),
+              videos: {
+                createMany: {
+                  data: matchedVideos.map((matchedVideo) => ({
+                    videoId: matchedVideo.id,
+                  })),
+                  skipDuplicates: true,
+                },
               },
             },
-          },
-        });
-      })
+          });
+        } else {
+          return prisma.tag.update({
+            where: { id: tag.id },
+            data: {
+              lastedMatchedAt: new Date(),
+            },
+          });
+        }
+      }),
+      10
     );
 
     return json(taggedVideos);
@@ -54,3 +64,23 @@ export async function loader({ params }) {
     prisma.$disconnect();
   }
 }
+
+const batchTransactions = async (
+  tags: ReturnType<typeof prisma.tag.update>[],
+  batchSize: number
+) => {
+  const batches: typeof tags[] = [];
+  for (let i = 0; i < tags.length; i += batchSize) {
+    batches.push(tags.slice(i, i + batchSize));
+  }
+
+  let transactionResults: Tag[] = [];
+  for (const batch of batches) {
+    console.log(batch.length);
+    transactionResults = transactionResults.concat(
+      await prisma.$transaction(batch)
+    );
+  }
+
+  return transactionResults;
+};
