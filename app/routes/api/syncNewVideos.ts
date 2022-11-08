520bb691ad99317915172ef25b37e3930c4d9769
Christian Cito <citochris@gmail.com>
2022-11-08 21:24:20 +0100

    fix(syncing): add error handling for 404 channels

--- app/routes/api/syncNewVideos.ts
+++ app/routes/api/syncNewVideos.ts
@@ -3,13 +3,13 @@ import { decode } from "html-entities";
 import { getChannel } from "~/sync/clients/youtubeRss.server";
 import { prisma } from "~/utils/prisma.server";
 
-export async function loader({ params }) {
+export async function loader() {
+  let errors: any[] = [];
   try {
-    const [channels, playlists, videos] = await Promise.all([
+    const [channels, videos] = await Promise.all([
       prisma.channel.findMany({
         select: { id: true, youtubeId: true, title: true },
       }),
-      prisma.playlist.findMany(),
       prisma.video.findMany({
         select: { id: true, youtubeId: true },
       }),
@@ -19,40 +19,49 @@ export async function loader({ params }) {
 
     const meta = await Promise.all([
       ...channels.map(async (channel) => {
-        const channelResponse = await getChannel(channel.youtubeId);
+        try {
+          const channelResponse = await getChannel(channel.youtubeId);
 
-        const updated = await prisma.$transaction(
-          channelResponse.items
-            .filter((item) => !videosYoutubeIds.includes(item.id))
-            .map((video) =>
-              prisma.video.upsert({
-                where: { youtubeId: video.id },
-                update: {},
-                create: {
-                  title: decode(video.title),
-                  youtubeId: video.id,
-                  publishedAt: video.pubDate,
-                  channel: { connect: { id: channel.id } },
-                },
-              })
-            )
-        );
+          const updated = await prisma.$transaction(
+            channelResponse.items
+              .filter((item) => !videosYoutubeIds.includes(item.id))
+              .map((video) =>
+                prisma.video.upsert({
+                  where: { youtubeId: video.id },
+                  update: {},
+                  create: {
+                    title: decode(video.title),
+                    youtubeId: video.id,
+                    publishedAt: video.pubDate,
+                    channel: { connect: { id: channel.id } },
+                  },
+                })
+              )
+          );
 
-        return { channel: { title: channel.title }, videos: updated };
+          return { channel: { title: channel.title }, videos: updated };
+        } catch (error) {
+          errors.push(error);
+          console.error(error);
+          return null;
+        }
       }),
     ]);
 
     return json(
-      meta.map((info) => {
-        return {
-          title: info.channel.title,
-          synced: info.videos.length,
-          items: info.videos.map((video) => video.title),
-        };
-      })
+      meta
+        .filter((info) => info !== null)
+        .map((info) => {
+          return {
+            title: info?.channel.title,
+            synced: info?.videos.length,
+            items: info?.videos.map((video) => video.title),
+          };
+        })
     );
   } catch (e) {
-    return json({ error: e }, 500);
+    console.error(e);
+    return json({ error: e, fetchErrors: errors }, 500);
   } finally {
     prisma.$disconnect();
   }
