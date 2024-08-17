dbced17f8fce46d03a87e6500a818414b97e2d5c
Christian Cito <citochris@gmail.com>
2024-08-17 20:44:15 +0200

    fix: respond with data

--- app/routes/api/ping-turso.ts
+++ app/routes/api/ping-turso.ts
@@ -1,22 +1,23 @@
 export async function loader() {
   try {
-    const data = await fetch(`https://hasanhub-arthouse.turso.io/v2/pipeline`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.TURSO_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        requests: [
-          { type: "execute", stmt: { sql: "SELECT * FROM Candidate" } },
-          { type: "close" },
-        ],
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+    const response = await fetch(
+      `https://hasanhub-arthouse.turso.io/v2/pipeline`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.TURSO_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          requests: [
+            { type: "execute", stmt: { sql: "SELECT * FROM Channel" } },
+            { type: "close" },
+          ],
+        }),
+      }
+    );
 
+    const data = await response.json();
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
     console.log(error);
