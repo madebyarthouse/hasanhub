6e197fe05deefbc8c47cbf985586c7b734906d04
Christian Cito <citochris@gmail.com>
2024-08-17 21:02:05 +0200

    fix: use failing quert

--- app/routes/api/ping-turso-prisma-query.ts
+++ app/routes/api/ping-turso-prisma-query.ts
@@ -1,12 +1,27 @@
-import getVideos from "~/lib/get-videos";
+import { Tag } from "@prisma/client";
+import { prisma } from "~/utils/prisma.server";
+
+type TagsForSidebar = (Tag & { viewsCount: number })[];
 
 export async function loader() {
   try {
-    const data = await getVideos({
-      order: "desc",
-      by: "publishedAt",
-    });
-    return new Response(JSON.stringify(data), { status: 200 });
+    const response = (await prisma.$queryRaw`
+    SELECT t.id, t.name, t.slug, sum(v.views) AS viewsCount
+    FROM Tag t
+      JOIN TagVideo tv ON tv.tagId = t.id
+      JOIN Video v ON tv.videoId = v.id  
+    GROUP BY t.id
+    ORDER BY viewsCount DESC
+  `) as unknown as TagsForSidebar;
+
+    console.log(response);
+
+    const tags = response.map((tag) => ({
+      ...tag,
+      viewsCount: Number(tag.viewsCount),
+    }));
+
+    return new Response(JSON.stringify(tags), { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response(JSON.stringify(error), { status: 500 });
