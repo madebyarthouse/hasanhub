32c98d33fdfc64df450b9240ff345ff659a9b207
Christian Cito <30470476+chrcit@users.noreply.github.com>
2026-02-03 12:28:38 +0100

    Clear repository for Cloudflare port

--- app/utils/dbEnums.ts
+++
@@ -1,9 +0,0 @@
-export const publishStatus = {
-  Published: "Published",
-  Unpublished: "Unpublished",
-};
-
-export const videoSyncStatus = {
-  Snippet: "Snippet",
-  Full: "Full",
-};
--- app/utils/debug.server.ts
+++
@@ -1,5 +0,0 @@
-export const debug = (message: any) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log(message);
-  }
-};
